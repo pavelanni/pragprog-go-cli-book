eeb5caee059ad74e943001e182c276bf2f8e434b
Pavel Anni <pavel.anni@sambanovasystems.com>
2021-12-29 20:30:05 -0500

    Complete CLI for pScan

--- cobra/pScan/cmd/scan.go
+++ cobra/pScan/cmd/scan.go
@@ -23,35 +23,69 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
+	"github.com/pavelanni/pragprog-go-cli-book/cobra/pScan/scan"
 	"github.com/spf13/cobra"
 )
 
 // scanCmd represents the scan command
 var scanCmd = &cobra.Command{
 	Use:   "scan",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Run a port scan on the hosts",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("scan called")
+		ports, err := cmd.Flags().GetIntSlice("ports")
+		if err != nil {
+			return err
+		}
+
+		return scanAction(os.Stdout, hostsFile, ports)
 	},
+}
+
+func scanAction(out io.Writer, hostsFile string, ports []int) error {
+	hl := &scan.HostsList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	results := scan.Run(hl, ports)
+
+	return printResults(out, results)
+}
+
+func printResults(out io.Writer, results []scan.Results) error {
+	message := ""
+
+	for _, r := range results {
+		message += fmt.Sprintf("%s:", r.Host)
+
+		if r.NotFound {
+			message += " Host not found\n\n"
+			continue
+		}
+
+		message += fmt.Sprintln()
+
+		for _, p := range r.PortStates {
+			message += fmt.Sprintf("\t%d: %s\n", p.Port, p.Open)
+		}
+
+		message += fmt.Sprintln()
+	}
+	_, err := fmt.Fprint(out, message)
+	return err
 }
 
 func init() {
 	rootCmd.AddCommand(scanCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// scanCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// scanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	scanCmd.Flags().IntSliceP("ports", "p", []int{22, 80, 443}, "ports to scan")
 }
