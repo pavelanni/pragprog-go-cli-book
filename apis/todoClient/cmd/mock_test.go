ce027b9d1cb398580e94c5ee5a57aa7b645cae9d
Pavel Anni <pavel.anni@sambanovasystems.com>
2022-01-18 10:52:23 -0500

    Add functions to todoClient

--- apis/todoClient/cmd/mock_test.go
+++ apis/todoClient/cmd/mock_test.go
@@ -64,6 +64,14 @@ var testResp = map[string]struct {
 		Status: http.StatusNotFound,
 		Body:   "404 - not found",
 	},
+	"created": {
+		Status: http.StatusCreated,
+		Body:   "",
+	},
+	"noContent": {
+		Status: http.StatusNoContent,
+		Body:   "",
+	},
 }
 
 // mockServer creates a mock server to simulate the todo API
